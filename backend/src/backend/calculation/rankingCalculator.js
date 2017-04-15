08347496573c1eb9b0f095ed0fd8d691b20d8298
thomas.kipar@gmail.com <thomas.kipar@gmail.com>
2017-04-15 17:27:36 +0200

    Added mock as ranking calculator

---
+++ backend/src/backend/calculation/rankingCalculator.js
@@ -0,0 +1,32 @@
+/**
+ * Created by thomas on 15.04.17.
+ */
+
+module.exports = {
+
+    calculateRanking : function() {
+        return {
+            ranking: [
+                {
+                    league: "London Rollergirls",
+                    wins: 7,
+                    losses: 1,
+                    points: 1002.4
+                },
+                {
+                    league: "Bear City Roller Derby",
+                    wins: 6,
+                    losses: 3,
+                    points: 332.7
+                },
+                {
+                    league: "Brewcity Bruisers",
+                    wins: 4,
+                    losses: 5,
+                    points: 213.4
+                }
+            ]
+        }
+    }
+    
+};
